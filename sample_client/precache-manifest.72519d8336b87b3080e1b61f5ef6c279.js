c1427ee582238c2c91355410bd22fde5fa9947e5
Jonathan David Estilo <jonathan.estilo@canon-europe.com>
2020-10-28 01:25:49 +0800

    Added sample client

---
+++ sample_client/precache-manifest.72519d8336b87b3080e1b61f5ef6c279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4f08c17434925719df2587fd837ddd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f87dd2dc12090216615",
+    "url": "/static/css/main.d84f6aab.chunk.css"
+  },
+  {
+    "revision": "74b92a1b57dd34deff7e",
+    "url": "/static/js/2.2c3b5a99.chunk.js"
+  },
+  {
+    "revision": "028d1b8f21ffff8caf0c799a051bd2b8",
+    "url": "/static/js/2.2c3b5a99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f87dd2dc12090216615",
+    "url": "/static/js/main.c20b7be6.chunk.js"
+  },
+  {
+    "revision": "a1cf1c9cb72720271280",
+    "url": "/static/js/runtime-main.7d17e208.js"
+  }
+]);
